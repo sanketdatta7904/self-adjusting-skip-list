4fab4dfbd1ea74b758c7193518394fe17290be39
sanketdatta123 <sanket.datta@american-technology.net>
2023-04-29 16:09:09 +0200

    Added promoto one level skiplist

--- main.py
+++ main.py
@@ -2,9 +2,10 @@
 from skiplist import  Skiplist
 from basic_skiplist import Basic_skiplist
 from promote_top_skiplist import Promote_top_skiplist
+from promote_one_level_skiplist import PromoteOneLevelSkiplist
 
 def test_insert():
-        s = Promote_top_skiplist()
+        s = PromoteOneLevelSkiplist()
         s.insert(5, "five")
         s.insert(2, "two")
         s.insert(8, "eight")
@@ -14,8 +15,8 @@ def test_insert():
         s.insert(7, "seven")
         s.insert(6, "six")
         s.display()
-        # s.findElementImp1(1)
-        s.findElementImp1(5)
+        s.findElementImp1(10)
+        # s.findElementImp1(5)
         s.display()
         
 
